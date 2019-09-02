02e8636ebc36abb826ec76b33fa4fd2d3d65c3ad
Elena Radiuk <13zone@gmail.com>
2019-09-02 19:03:57 +0300

    rp 1

---
+++ react-project-1/src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import AppHeader from "./component/AppHeader";
+import Footer from "./component/Footer";
+
+function App() {
+    return (
+
+    <div className="App">
+        <AppHeader></AppHeader>
+
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        </header>
+
+        <Footer></Footer>
+
+
+    </div>
+    );
+}
+
+export default App;
---
+++ react-project-1/src/component/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function Footer() {
+    return (
+     <footer>
+
+
+    <div class="footer-line-bottom">
+        <div class="footer-line-bottom-content">
+        <p class="footer-line-bottom-left">Copyright © 2018 hysucat. all rights reserved</p>
+    <p class="footer-line-bottom-right">Terms & Conditions | Privacy Policy</p>
+        </div>
+        </div>
+    </footer>
+)
+}
+
+export default Footer
