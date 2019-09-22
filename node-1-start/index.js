a9dc11dd0522cb8e5c8b099f91a540192af27da6
ElenaRadiuk <olena.radiuk@gmail.com>
2019-09-22 22:54:21 +0300

    node express

--- node-1-start/index.js
+++ node-1-start/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
 
-app.use() 
+app.use('/static', express.static(__dirname + "/public"));
+
+
+
+app.use("/", function(req, res, next){
+    console.log('middleware');
+    next();
+    // res.sendFile(__dirname + "/public/about.html")
+})
+
+app.use("/about", function(req, res){
+    let id = req.query.id;
+    let user = req.query.user;
+    res.send("<h2></h2><p>id=" + id + " user=" + user + "</p>");
+    // http://localhost:3007/about?id=10&user=Super 
+    // id=10 user=Super
+})
 
 app.get('/', function(req, res){
     res.send('Hello2');
